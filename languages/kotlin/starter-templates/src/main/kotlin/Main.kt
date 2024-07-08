ae30af02bd74f7726e4f3181d4fb3bc1d39a515f
Paul Kuruvilla <rohitpaulk@gmail.com>
2024-07-08 13:59:15 +0100

    Add Kotlin starter templates for CodeCrafters

---
+++ languages/kotlin/starter-templates/src/main/kotlin/Main.kt
@@ -0,0 +1,6 @@
+fun main(args: Array<String>) {
+    // You can use print statements as follows for debugging, they'll be visible when running tests.
+    System.err.println("Logs from your program will appear here!")
+
+    println("Hello world!")
+}
