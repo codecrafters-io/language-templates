174d1cbee350faca98d1e9b9f4f57ff5455044d7
Paul Kuruvilla <rohitpaulk@gmail.com>
2024-08-13 12:47:59 +0100

    add src

---
+++ languages/zig/code/src/main.zig
@@ -0,0 +1,9 @@
+const std = @import("std");
+
+pub fn main() !void {
+    // You can use print statements as follows for debugging, they'll be visible when running tests.
+    std.debug.print("Logs from your program will appear here!\n", .{});
+
+    // Uncomment this block to pass the first stage
+    // std.debug.print("Logs from your program will appear here!\n", .{});
+}
