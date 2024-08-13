a33d0ae35a73dc40cbcb952a12a52ffd8bed41d4
Paul Kuruvilla <rohitpaulk@gmail.com>
2024-08-13 15:34:40 +0100

    Add starter code for the first stage

--- languages/elixir/code/lib/main.ex
+++ languages/elixir/code/lib/main.ex
@@ -1,4 +1,5 @@
 # You can use print statements as follows for debugging, they'll be visible when running tests.
 IO.puts("Logs from your program will appear here!")
 
+# Uncomment this block to pass the first stage
 # TODO: Add starter code here
