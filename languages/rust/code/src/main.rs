76b506b7b0ed50e9c780feb0479a1324e657ec6e
Paul Kuruvilla <rohitpaulk@gmail.com>
2024-08-22 19:52:11 +0100

    Add initial Rust main file and update required cargo version to 1.80

---
+++ languages/rust/code/src/main.rs
@@ -0,0 +1,9 @@
+use std::io::{self, Write};
+
+fn main() {
+    // You can use print statements as follows for debugging, they'll be visible when running tests.
+    writeln!(io::stderr(), "Logs from your program will appear here!").unwrap();
+
+    // Uncomment this block to pass the first stage
+    // println!("TODO: Implement starter code");
+}
