7a33d0ed5b2f33880c8ae31d39dfc3dd1fd760da
Andy Li <1450947+andy1li@users.noreply.github.com>
2024-09-30 20:47:49 +0800

    Add java template

---
+++ languages/java/code/src/main/java/Main.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Main {
+  public static void main(String[] args){
+    // You can use print statements as follows for debugging, they'll be visible when running tests.
+    System.out.println("Logs from your program will appear here!");
+
+    //  Uncomment this block to pass the first stage
+    // System.out.println("TODO: Implement starter code");
+  }
+}
