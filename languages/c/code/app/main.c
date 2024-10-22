1e29c64b434019806b0c675974d3ea6ffba71aaa
Andy Li <1450947+andy1li@users.noreply.github.com>
2024-10-22 20:03:04 +0800

    Add c-14.2

---
+++ languages/c/code/app/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    // You can use print statements as follows for debugging, they'll be visible when running tests.
+    fprintf(stderr, "Logs from your program will appear here!\n");
+
+    // Uncomment this block to pass the first stage
+    // printf("TODO: Implement starter code\n");
+
+    return 0;
+}
