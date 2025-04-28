990826c29d536a505c16fbf74992957625d0913e
Andy Li <1450947+andy1li@users.noreply.github.com>
2025-04-28 16:32:03 +0800

    Add Python project structure and configuration files
    - Introduce configuration file (config.yml) specifying required executable and user-editable file.
    - Create .gitignore to exclude common Python artifacts and directories.
    - Add compile.sh and run.sh scripts for CodeCrafters integration.
    - Implement a basic main.py file with a placeholder for user code.
    - Establish Dockerfile for Python 3.13 Alpine image.

---
+++ languages/python/code/app/main.py
@@ -0,0 +1,13 @@
+import sys
+
+
+def main():
+    # You can use print statements as follows for debugging, they'll be visible when running tests.
+    print("Logs from your program will appear here!", file=sys.stderr)
+
+    # Uncomment this to pass the first stage
+    # print("Implement code here!")
+
+
+if __name__ == "__main__":
+    main()
