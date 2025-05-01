991e97531d83b386b4dd287627932afc0a3845c7
Andy Li <1450947+andy1li@users.noreply.github.com>
2025-05-02 01:22:24 +0800

    Add C# project structure and update configuration
    - Introduce new C# project files: codecrafters-{{course_slug}}.csproj and codecrafters-{{course_slug}}.sln for project management.
    - Update config.yml to change the user-editable file path from src/main.cs to src/Program.cs.
    - Modify compile.sh and run.sh scripts to reference the updated project file name.
    - Create a basic Program.cs file with initial logging for user code.

--- languages/csharp/code/.codecrafters/run.sh
+++ languages/csharp/code/.codecrafters/run.sh
@@ -8,4 +8,4 @@
 
 set -e # Exit on failure
 
-exec /tmp/codecrafters-build-csharp/codecrafters-{{course_slug_underscorized}} "$@"
+exec /tmp/codecrafters-build-csharp/codecrafters-{{course_slug}} "$@"
