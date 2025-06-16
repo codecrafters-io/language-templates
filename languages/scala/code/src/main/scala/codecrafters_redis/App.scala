103b712ce14b857ac0d9eea462aefe0a3874e3ee
Andy Li <1450947+andy1li@users.noreply.github.com>
2025-06-16 09:43:59 +0800

    refactor(scala): simplify App.scala by removing the Server object and updating the main entry point

--- languages/scala/code/src/main/scala/codecrafters_redis/App.scala
+++ languages/scala/code/src/main/scala/codecrafters_redis/App.scala
@@ -1,16 +1,9 @@
 package codecrafters_{{course_slug_underscorized}}
 
-import java.net.{InetSocketAddress, ServerSocket}
+object Main extends App {
+  // You can use print statements as follows for debugging, they'll be visible when running tests.
+  System.err.println("Logs from your program will appear here!")
 
-object Server {
-  def main(args: Array[String]): Unit = {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println("Logs from your program will appear here!")
-
-    // Uncomment this to pass the first stage
-    //
-    // val serverSocket = new ServerSocket()
-    // serverSocket.bind(new InetSocketAddress("localhost", 6379))
-    // val clientSocket = serverSocket.accept() // wait for client
-  }
+  // Uncomment this to pass the first stage
+  // System.out.println("TODO: Implement starter code");
 }
