9ee783cbb01befb07221beb1827fab2b8ee00250
TropicolX <jacobsbusayo@gmail.com>
2026-02-24 18:20:15 +0100

    add dart template

---
+++ languages/dart/code/bin/main.dart
@@ -0,0 +1,9 @@
+import 'dart:io';
+
+void main(List<String> arguments) {
+  // You can use print statements as follows for debugging, they'll be visible when running tests.
+  stderr.writeln('Logs from your program will appear here!');
+
+  // TODO: Uncomment the following line to pass the first stage
+  // print("Implement code here!");
+}
