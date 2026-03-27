273bf409ccb742665238b8277d5d765d02f94152
Andy Li <1450947+andy1li@users.noreply.github.com>
2026-03-27 16:23:13 +0900

    Refactor Scala configuration and build process to use scala-cli instead of sbt; update Dockerfile and scripts accordingly.

--- languages/scala/code/src/main/scala/codecrafters_{{course_slug_underscorized}}/App.scala
+++ languages/scala/code/src/main/scala/codecrafters_{{course_slug_underscorized}}/App.scala
@@ -1,9 +1,9 @@
 package codecrafters_{{course_slug_underscorized}}
 
-object Main extends App {
+@main def main(): Unit = {
   // You can use print statements as follows for debugging, they'll be visible when running tests.
   System.err.println("Logs from your program will appear here!")
 
   // TODO: Uncomment the code below to pass the first stage
-  // System.out.println("TODO: Implement starter code");
+  // System.out.println("TODO: Implement starter code")
 }
