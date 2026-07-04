32c8cea00c4b2e961d1e78382714874dbe7ebf38
Ihar Tsalka <git@tsobako.me>
2026-07-04 21:00:43 +0300

    Fix sample code for Ada

--- languages/ada/code/src/main.adb
+++ languages/ada/code/src/main.adb
@@ -2,5 +2,9 @@ with Ada.Text_IO;
 
 procedure Main is
 begin
-   Ada.Text_IO.Put_Line ("Logs from your program will appear here!");
+   Ada.Text_IO.Put_Line(Ada.Text_IO.Standard_Error, "Logs from your program will appear here!");
+
+   -- TODO: Uncomment the code below to pass the first stage
+   -- Ada.Text_IO.Put_Line("TODO: Implement starter code\n");
+
 end Main;
